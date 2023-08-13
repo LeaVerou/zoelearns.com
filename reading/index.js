203e28a1aa0e3604416e335fa0b32edb5fcd06f5
Lea Verou <lea@verou.me>
2023-08-13 06:47:40 +0300

    reading prototype to VueJS

--- reading/index.js
+++ reading/index.js
@@ -1,193 +1,71 @@
-import getPhotos from "../util/get-photos.js";
+import { createApp } from 'https://unpkg.com/vue@3/dist/vue.esm-browser.js';
+import WordCard from "./word-card.js";
 
 const vowels = "αεηιουω";
 const consonants = "βγδζθκλμνξπρσςτφχψ";
-const word_info = await (await fetch("words.json")).json();
-const words = Object.keys(word_info);
+const all_words = await (await fetch("words.json")).json();
 
-let methods = {
-	dropAccents (word) {
-		return word.normalize("NFD").replace(/[\u0300-\u036f]/g, "");
-	},
-
-	syllabize(word) {
-		let syllables = [];
-		let syllable = "";
-
-		// Drop accents
-		let normalizedWord = methods.dropAccents(word);
-
-		for (let i = 0; i < normalizedWord.length; i++) {
-			let letter = normalizedWord[i];
-			let originalLetter = word[i];
-			syllable += originalLetter;
-
-			if (vowels.includes(letter)) {
-				syllables.push(syllable);
-				syllable = "";
-			}
-		}
-
-		if (syllable.length > 0) {
-			syllables.push(syllable);
-		}
-
-		return syllables;
-	},
-
-	gotoSyllable(offset) {
-		let word = document.querySelector(".word");
-		let activeSyllable = word.querySelector(".syllable.active");
-
-		if (activeSyllable) {
-			activeSyllable.classList.remove("active");
-
-			// Make next syllable active
-			let nextSyllable = activeSyllable[offset < 0? "previousElementSibling" : "nextElementSibling"];
-			if (nextSyllable) {
-				nextSyllable.classList.add("active");
-			}
-		}
-		else {
-			// Make first syllable active
-			let firstSyllable = word.querySelector(".syllable");
-			firstSyllable.classList.add("active");
-		}
-	},
-
-	gotoWord(offset) {
-		let currentIndex = parseInt(document.body.dataset.word);
-		let newIndex = currentIndex + offset;
-
-		// Roll index around
-		if (newIndex < 0) {
-			newIndex = words.length + newIndex;
-		}
-		else if (newIndex >= words.length) {
-			newIndex = newIndex - words.length;
-		}
-
-		document.body.dataset.word = newIndex;
-		readWord(newIndex);
-	},
-
-	async correct() {
-		// Remove active syllable, if present
-		document.querySelector(".syllable.active")?.classList.remove("active");
-
-		let word = document.querySelector(".word").textContent;
-		let info = word_info[word];
-
-		if (!info) {
-			throw new Error(`No info for word ${word}`);
-		}
-
-		let json = await getPhotos(info.en, {per_page: 4});
-		let totalAspectRatio = 0;
-		let photos = json.results.map(photo => {
-			totalAspectRatio += photo.width / photo.height;
-			return Object.assign(document.createElement("img"), {
-				src: photo.urls.small,
-				alt: photo.description,
-				className: "photo",
-				style: Object.entries({
-					"--color": photo.color,
-					"--width": photo.width,
-					"--height": photo.height
-				}).map(([key, value]) => `${key}: ${value}`).join("; ")
-			});
-		});
-
-		document.querySelector(".photos").style.setProperty("--total-aspect-ratio", totalAspectRatio);
-
-		document.querySelector(".photos").append(...photos);
-	},
-
-	wrong() {
-
-	}
+for (let word in all_words) {
+	all_words[word].word = word;
 }
 
-// Shortcuts
-Object.assign(methods, {
-	nextSyllable: methods.gotoSyllable.bind(null, 1),
-	previousSyllable: methods.gotoSyllable.bind(null, -1),
-	nextWord: methods.gotoWord.bind(null, 1),
-	previousWord: methods.gotoWord.bind(null, -1),
-});
+globalThis.app = createApp({
+	data() {
+		let all_words_array = Object.values(all_words);
 
+		return {
+			all_words: all_words_array,
+			unused_words: all_words_array.slice(),
+			used_words: [],
+		}
+	},
 
-function readWord(wordOrIndex) {
-	let word = wordOrIndex;
-	if (typeof wordOrIndex === "number") {
-		document.body.dataset.word = wordOrIndex;
-		word = words[wordOrIndex];
-	}
+	mounted () {
+		this.next_word();
+	},
 
-	let syllables = methods.syllabize(word);
-	let formattedSyllables = syllables.map(syllable => {
-		let formattedSyllable = [...syllable].map(letter => {
-			let normalizedLetter = methods.dropAccents(letter);
-			let index = normalizedLetter.charCodeAt(0) - "α".charCodeAt(0);
-			return `<span class="letter" style="--index: ${index}">${letter}</span>`
-		}).join("");
-		return `<span class="syllable">${formattedSyllable}</span>`;
-	});
+	computed: {
+		// all_words_array() {
+		// 	return Object.values(this.all_words);
+		// }
+	},
 
-	let article = document.createElement("article");
-	article.classList.add("word-card");
-	article.innerHTML = `
-		<div class="toolbar">
-			<button class="previous-word">◀◀</button>
-			<button class="previous">◀</button>
-			<div class="spacer"></div>
-			<button class="correct">✓</button>
-			<button class="wrong">✗</button>
-			<div class="spacer"></div>
-			<button class="next">▶</button>
-			<button class="next-word">▶▶</button>
-		</div>
-		<h2 class="word">${ formattedSyllables.join("") }</h2>
-		<div class="photos"></div>
-	`;
+	methods: {
+		next_word (wordOrIndex) {
+			if (this.used_words.length < this.all_words.length) {
+				let i, word;
 
-	document.addEventListener("keydown", evt => {
-		if (evt.key === "ArrowLeft" || evt.key === "ArrowUp") {
-			if (evt.shiftKey) {
-				methods.previousWord();
+				if (typeof wordOrIndex === "number") {
+					i = wordOrIndex;
+				}
+				else if (wordOrIndex) {
+					i = this.unused_words.findIndex(word => word.word === wordOrIndex);
+
+					if (i === -1) {
+						// what if the word we want to read is used?
+						i = this.used_words.findIndex(word => word.word === wordOrIndex);
+						word = this.unused_words.splice(i, 1)[0];
+					}
+				}
+				else {
+					// Pick a random word
+					i = Math.floor(Math.random() * this.unused_words.length);
+				}
+
+				word = word ?? this.unused_words.splice(i, 1)[0];
+				this.used_words.push(word);
 			}
 			else {
-				methods.previousSyllable();
+				// TODO communicate that we ran out of words
 			}
-		}
-		else if (evt.key === "ArrowRight" || evt.key === "ArrowDown") {
-			if (evt.shiftKey) {
-				methods.nextWord();
-			}
-			else {
-				methods.nextSyllable();
-			}
-		}
-		else if (evt.key === "Enter") {
-			methods.correct();
-		}
-	});
+		},
+	},
 
-	let buttons = {
-		previous: article.querySelector(".previous"),
-		next: article.querySelector(".next"),
-		previousWord: article.querySelector(".previous-word"),
-		nextWord: article.querySelector(".next-word"),
-		correct: article.querySelector(".correct"),
-		wrong: article.querySelector(".wrong")
+	components: {
+		"word-card": WordCard
+	},
+
+	isCustomElement: tag => {
+		return !(tag in this.components)
 	}
-
-	for (let button in buttons) {
-		buttons[button].addEventListener("click", methods[button]);
-	}
-
-	document.body.append(article);
-}
-
-
-readWord(3);
+}).mount(document.body);
---
+++ reading/word-card.js
@@ -0,0 +1,148 @@
+import getPhotos from "../util/get-photos.js";
+
+const vowels = "αεηιουω";
+const consonants = "βγδζθκλμνξπρσςτφχψ";
+
+export default {
+	props: {
+		word: Object,
+		active: Boolean
+	},
+
+	data() {
+		return {
+			current_syllable: -1,
+			status: "",
+			photos: []
+		}
+	},
+
+	computed: {
+		syllables () {
+			return this.word.syllables ?? this.syllabize(this.word.word);
+		},
+
+		normalizedWord () {
+			return this.dropAccents(this.word.word);
+		}
+	},
+
+	methods: {
+		dropAccents (word = this.word.word) {
+			return word.normalize("NFD").replace(/[\u0300-\u036f]/g, "");
+		},
+
+		syllabize (word = this.word.word) {
+			let syllables = [];
+			let syllable = "";
+
+			// Drop accents
+			let normalizedWord = this.dropAccents(word);
+
+			for (let i = 0; i < normalizedWord.length; i++) {
+				let letter = normalizedWord[i];
+				let originalLetter = word[i];
+				syllable += originalLetter;
+
+				if (vowels.includes(letter)) {
+					syllables.push(syllable);
+					syllable = "";
+				}
+			}
+
+			if (syllable.length > 0) {
+				syllables.push(syllable);
+			}
+
+			return syllables;
+		},
+
+		goto_syllable (offset) {
+			let newIndex = this.current_syllable + offset;
+
+			if (newIndex === this.syllables.length) {
+				newIndex = -1;
+			}
+			else if (newIndex < 0) {
+				newIndex = this.syllables.length + 1 + newIndex;
+			}
+
+			this.current_syllable = newIndex;
+		},
+
+		previous_syllable () {
+			return this.goto_syllable(-1);
+		},
+
+		next_syllable () {
+			return this.goto_syllable(1);
+		},
+
+		async correct () {
+			// Remove active syllable, if present
+			this.current_syllable = -1;
+			this.status = "correct";
+			this.photos = (await getPhotos(this.word.en, {per_page: 4})).results;
+		},
+
+		next_word () {
+			this.status ||= "skipped";
+			this.$emit("next_word");
+		},
+
+		handleEvent (evt) {
+			if (evt.key === "ArrowLeft" || evt.key === "ArrowUp") {
+				this.previous_syllable();
+			}
+			else if (evt.key === "ArrowRight" || evt.key === "ArrowDown") {
+				if (evt.shiftKey) {
+					this.next_word();
+				}
+				else {
+					this.next_syllable();
+				}
+			}
+			else if (evt.key === "Enter") {
+				this.correct();
+			}
+		}
+	},
+
+	watch: {
+		active: {
+			handler (active) {
+				if (active) {
+					document.addEventListener("keydown", this);
+				}
+				else {
+					document.removeEventListener("keydown", this);
+				}
+			},
+			immediate: true
+		}
+	},
+
+	template: `
+		<article class="word-card" :class="[status, active? 'active' : '']">
+			<div class="toolbar">
+				<button title="Previous syllable" class="previous" @click="previous_syllable">◀</button>
+				<div class="spacer"></div>
+				<button class="correct" @click="correct" v-if="status !== 'correct'">✓</button>
+				<button class="next-word" @click.stop="next_word">▶▶</button>
+				<div class="spacer"></div>
+				<button title="Next syllable" class="next">▶</button>
+
+			</div>
+			<h2 class="word">
+				<span class="syllable" v-for="(syllable, i) in syllables" :class="{active: i === current_syllable}">
+					<span v-for="letter in syllable" class="letter"
+					:style="{'--index': letter.charCodeAt(0) - 'α'.charCodeAt(0)}">{{ letter }}</span>
+				</span>
+			</h2>
+			<div class="photos" v-if="status === 'correct'" :style="{ '--total-aspect-ratio': photos.reduce((a, c) => a + c.width / c.height, 0) || 4 }">
+				<img v-for="photo in photos" :src="photo.urls.small" :alt="photo.description"
+				:style="{ '--color': photo.color, '--aspect-ratio': photo.width / photo.height }" />
+			</div>
+		</article>
+	`
+}
