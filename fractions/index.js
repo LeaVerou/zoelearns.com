28231ff3c681518d3b3f1625159015dd3de25b96
Lea Verou <lea@verou.me>
2023-12-16 17:34:57 -0500

    Prototype fractions > 1 (sizing still needs work, esp. on portrait layouts)

--- fractions/index.js
+++ fractions/index.js
@@ -6,7 +6,35 @@ globalThis.app = createApp({
 	data () {
 		return {
 			numerator: 2,
-			denominator: 3
+			denominator: 3,
 		}
-	}
+	},
+
+	computed: {
+		fraction () {
+			return this.numerator / this.denominator;
+		},
+
+		pies () {
+			return Math.ceil(this.fraction);
+		},
+
+		full_pies () {
+			return Math.floor(this.fraction);
+		},
+
+		pies_x () {
+			return Math.ceil(Math.sqrt(this.pies));
+		},
+
+		app_styles () {
+			return {
+				"--numerator": this.numerator,
+				"--denominator": this.denominator,
+				"--pies": this.pies,
+				"--pies-x": this.pies_x,
+				"--pies-y": Math.ceil(this.pies / this.pies_x),
+			}
+		}
+	},
 }).mount(document.body);
