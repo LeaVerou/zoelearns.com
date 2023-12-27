d523e0fcf910c9c379510eda7bb59021bd7342cc
Lea Verou <lea@verou.me>
2023-12-27 02:06:43 +0200

    [reading] Add English

---
+++ reading/langs/en/lang.js
@@ -0,0 +1,8 @@
+// import { dropAccents } from "../../util.js";
+
+export const code = "en-GB";
+export const READING_GRANULARITY = "phoneme";
+export const VOWELS_NOACCENTS = "aeiou";
+
+export const DIGRAPH_VOWELS = ["ee", "oo", "ai", "ay", "ea", "ee", "ei", "ey", "ie", "oa", "oe", "oi", "oo", "ou", "oy", "ue", "ui", "uy"];
+export const DIGRAPH_CONSONANTS = ["ch", "ck", "gh", "gn", "kn", "ng", "ph", "qu", "rh", "sc", "sh", "th", "wh", "wr"];
