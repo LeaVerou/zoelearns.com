db065284ffc98b74a9166516d1ac2a48137b512d
Lea Verou <lea@verou.me>
2024-04-21 20:12:06 -0400

    [writing] Initial commit

---
+++ writing/index.js
@@ -0,0 +1,87 @@
+import Vue from "../common/vue.js";
+import settings from "../common/settings.js";
+
+let { createApp, nextTick } = Vue;
+
+globalThis.app = createApp({
+	data () {
+		return {
+			lettersSource: "A, a, B, b, C, c, D, d, E, e, F, f, G, g, H, h, I, i, J, j, K, k, L, l, M, m, N, n, O, o, P, p, Q, q, R, r, S, s, T, t, U, u, V, v, W, w, X, x, Y, y, Z, z",
+			// size: 36,
+			lines_per_page: 12,
+			repetitions: 1,
+			page_size: "letter",
+			orientation: "portrait",
+			show_settings: false,
+			default_settings: {
+
+			},
+		}
+	},
+
+	mixins: [
+		settings
+	],
+
+	computed: {
+		letters () {
+			return this.lettersSource.split(",").map(letter => letter.trim());
+		},
+		lettersDrawn () {
+			return this.letters.flatMap(letter => Array(this.repetitions).fill(letter));
+		},
+		pageHeight () {
+			// pt to inches * height in inches * % of page that is not margin
+			return 72 * (this.page_size === "letter" ? 11 : 297 / 25.4) * .9;
+		},
+		size () {
+			// lines_per_page = this.pageHeight / (this.size * 1.3)
+			return Math.floor(this.pageHeight / (this.lines_per_page * 1.3));
+		},
+		// lines_per_page () {
+		// 	return this.pageHeight / (this.size * 1.3);
+		// },
+		pages () {
+			let { lettersDrawn, lines_per_page } = this;
+
+			let pageCount = Math.floor(lettersDrawn.length / lines_per_page);
+			let ret = [];
+			for (let i=0; i<pageCount; i++) {
+				ret.push(lettersDrawn.slice(i * lines_per_page, (i+1) * lines_per_page));
+			}
+			return ret;
+		},
+	},
+
+	watch: {
+		size: {
+			handler (value) {
+				document.documentElement.style.setProperty("--size", value);
+				// this.updateLineHeight();
+			},
+			immediate: true
+		},
+		page_size: {
+			handler (value) {
+				document.documentElement.style.setProperty("--page-size", value);
+			},
+			immediate: true
+		},
+		orientation: {
+			handler (value) {
+				document.documentElement.style.setProperty("--orientation", value);
+			},
+			immediate: true
+		},
+	},
+
+	methods: {
+		print() {
+			window.print();
+		},
+		// async updateLineHeight () {
+		// 	await nextTick();
+		// 	this.lineHeight = document.querySelector(".letter-line")?.offsetHeight;
+		// }
+	},
+}).mount(document.body);
