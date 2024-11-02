8122d4f97f2c581d8b99ee2fbd029d0c376e0be0
Lea Verou <lea@verou.me>
2024-11-02 18:32:56 -0400

    Customizable fraction worksheet

--- fractions/worksheet/src/index.js
+++ fractions/worksheet/src/index.js
@@ -1,1 +1,28 @@
+import Vue from "../../../common/vue.js";
 import "./fraction-exercise.js";
+
+let { createApp } = Vue;
+
+globalThis.app = createApp({
+	data () {
+		return {
+			exercises: [
+				{ fraction: "1/2" },
+				{ fraction: "1/3" },
+				{ fraction: "/4" },
+				{ max: "/5" },
+				{ max: "10/5" },
+			]
+		}
+	},
+
+	computed: {
+
+	},
+
+	methods: {
+		print() {
+			window.print();
+		}
+	}
+}).mount(document.body);
