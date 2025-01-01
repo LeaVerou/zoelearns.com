311a3bd8cc413f41c4319ef03f08c812573924aa
Lea Verou <lea@verou.me>
2025-01-01 18:02:27 -0500

    Move `importWithFallbacks()` within `vue.js`
    Since that was the only file using it

--- common/vue.js
+++ common/vue.js
@@ -1,4 +1,23 @@
-import { importWithFallbacks } from "./util.js";
+
+
+ async function importWithFallbacks (...specifiers) {
+	for (let i = 0; i < specifiers.length; i++) {
+		let specifier = specifiers[i];
+
+		if (typeof specifier !== "string") {
+			// This allows us to end the chain with actual data / functions
+			return specifier;
+		}
+
+		try {
+			return await import(specifier)
+		} catch (error) {
+			if (i === specifiers.length - 1) {
+				throw error;
+			}
+		}
+	}
+}
 
 export default await importWithFallbacks(
 	"../node_modules/vue/dist/vue.esm-browser.js",
